18c5e65d891d6c4dcb2bd083b3b3cc97d6bff011
Budi Santoso <sebuah.email@gmail.com>
2024-11-27 02:25:42 +0700

    [patch]DatabaseConfigImpl

--- src/main/java/dev/boenkkk/simulator_outstation_dnp3/config/DatabaseConfigImpl.java
+++ src/main/java/dev/boenkkk/simulator_outstation_dnp3/config/DatabaseConfigImpl.java
@@ -1,5 +1,6 @@
 package dev.boenkkk.simulator_outstation_dnp3.config;
 
+import dev.boenkkk.simulator_outstation_dnp3.util.TimeUtil;
 import io.stepfunc.dnp3.*;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,29 +12,51 @@ public class DatabaseConfigImpl {
 
     // ANCHOR: database_init_function
     public static void initializeDatabaseDefault(Database db) {
+        Flags flags = new Flags(Flag.ONLINE);
+        Timestamp timeNow = TimeUtil.now();
+        UpdateOptions updateOptions = UpdateOptions.detectEvent();
+
         // define device attributes made available to the master
         db.defineStringAttr(ubyte(0), false, AttributeVariations.USER_ASSIGNED_OWNER_NAME, "BOENKKK.DEV");
         db.defineStringAttr(ubyte(0), true, AttributeVariations.PRODUCT_NAME_AND_MODEL, "SIMULATOR DNP3 OUTSTATION");
 
-        db.addBinaryOutputStatus(ushort(0), EventClass.CLASS1, new BinaryOutputStatusConfig());
+        for (int i= 0; i< 3; i++){
+            // TODO: NOTE
+            // 0: Local / Remote
+            // 1: CB Open / Close
+            BinaryInputConfig binaryInputConfig = new BinaryInputConfig()
+                .withStaticVariation(StaticBinaryInputVariation.GROUP1_VAR2)
+                .withEventVariation(EventBinaryInputVariation.GROUP2_VAR2);
+            db.addBinaryInput(ushort(i), EventClass.CLASS1, binaryInputConfig);
 
-        // add 10 points of each type
-        for (int i = 0; i < 3; i++) {
-            // you can explicitly specify the configuration for each point ...
-            db.addBinaryInput(ushort(i), EventClass.CLASS1, new BinaryInputConfig(StaticBinaryInputVariation.GROUP1_VAR1, EventBinaryInputVariation.GROUP2_VAR2));
-            // ... or just use the defaults
-            db.addDoubleBitBinaryInput(ushort(i), EventClass.CLASS1, new DoubleBitBinaryInputConfig());
-            // db.addBinaryOutputStatus(ushort(i), EventClass.CLASS1, new BinaryOutputStatusConfig());
-            db.addCounter(ushort(i), EventClass.CLASS1, new CounterConfig());
-            db.addFrozenCounter(ushort(i), EventClass.CLASS1, new FrozenCounterConfig());
-            db.addAnalogInput(ushort(i), EventClass.CLASS1, new AnalogInputConfig());
-            db.addAnalogOutputStatus(ushort(i), EventClass.CLASS1, new AnalogOutputStatusConfig());
-            db.addOctetString(ushort(i), EventClass.CLASS1);
+            BinaryInput binaryInput = new BinaryInput(ushort(i), false, flags, timeNow);
+            db.updateBinaryInput(binaryInput, updateOptions);
         }
 
-        // define device attributes made available to the master
-        db.defineStringAttr(ubyte(0), false, AttributeVariations.DEVICE_MANUFACTURERS_NAME, "Step Function I/O");
-        db.defineStringAttr(ubyte(0), true, AttributeVariations.USER_ASSIGNED_LOCATION, "Bend, OR");
+        for (int i = 0; i < 2; i++) {
+            // TODO: NOTE
+            // 0: AI0 Up
+            // 1: AI0 Down
+            BinaryOutputStatusConfig binaryOutputStatusConfig = new BinaryOutputStatusConfig()
+                .withStaticVariation(StaticBinaryOutputStatusVariation.GROUP10_VAR2)
+                .withEventVariation(EventBinaryOutputStatusVariation.GROUP11_VAR2);
+            db.addBinaryOutputStatus(ushort(i), EventClass.CLASS1, binaryOutputStatusConfig);
+
+            BinaryOutputStatus binaryOutputStatus = new BinaryOutputStatus(ushort(i), false, flags, timeNow);
+            db.updateBinaryOutputStatus(binaryOutputStatus, updateOptions);
+        }
+
+        for (int i = 0; i < 1; i++) {
+            // TODO: NOTE
+            // 0: Simulate Tap Changer
+            AnalogInputConfig analogInputConfig = new AnalogInputConfig()
+                .withStaticVariation(StaticAnalogInputVariation.GROUP30_VAR4)
+                .withEventVariation(EventAnalogInputVariation.GROUP32_VAR4);
+            db.addAnalogInput(ushort(i), EventClass.CLASS1, analogInputConfig);
+
+            AnalogInput analogInput = new AnalogInput(ushort(i), 0.0, flags, timeNow);
+            db.updateAnalogInput(analogInput, UpdateOptions.detectEvent());
+        }
     }
     // ANCHOR_END: database_init_function
 }

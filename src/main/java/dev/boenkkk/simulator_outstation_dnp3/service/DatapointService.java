a8b456fc932d05dc893fb4c9e0971a50d9f8b963
Budi Santoso <sebuah.email@gmail.com>
2024-11-27 23:24:07 +0700

    [patch]DatapointService -add updateValueBinaryOutput -add getAnalogInput -add updateValueAnalogInput

--- src/main/java/dev/boenkkk/simulator_outstation_dnp3/service/DatapointService.java
+++ src/main/java/dev/boenkkk/simulator_outstation_dnp3/service/DatapointService.java
@@ -1,10 +1,7 @@
 package dev.boenkkk.simulator_outstation_dnp3.service;
 
 import dev.boenkkk.simulator_outstation_dnp3.util.TimeUtil;
-import io.stepfunc.dnp3.BinaryInput;
-import io.stepfunc.dnp3.Flag;
-import io.stepfunc.dnp3.Flags;
-import io.stepfunc.dnp3.UpdateOptions;
+import io.stepfunc.dnp3.*;
 import lombok.extern.slf4j.Slf4j;
 import org.joou.UShort;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +38,47 @@ public class DatapointService {
                         db.updateBinaryInput(binaryInput, UpdateOptions.detectEvent());
                     }
                 );
-            });
+            }
+        );
+    }
+
+    public void updateValueBinaryOutput(String endpoint, Integer index, Boolean value){
+        outstationsService.getoOutstationData(endpoint).ifPresent(
+            outstationData -> {
+                outstationData.getOutstation().transaction(
+                    db -> {
+                        BinaryOutputStatus binaryOutputStatus = new BinaryOutputStatus(UShort.valueOf(index), value, new Flags(Flag.ONLINE), TimeUtil.now());
+                        db.updateBinaryOutputStatus(binaryOutputStatus, UpdateOptions.detectEvent());
+                    }
+                );
+            }
+        );
+    }
+
+    public Double getAnalogInput(String enpoint, Integer index){
+        AtomicReference<Double> returnValue = new AtomicReference<>(0.0);
+        outstationsService.getoOutstationData(enpoint).ifPresent(
+            outstationData -> {
+                outstationData.getOutstation().transaction(
+                    db -> {
+                        returnValue.set(db.getAnalogInput(UShort.valueOf(index)).value);
+                    }
+                );
+            }
+        );
+        return returnValue.get();
+    }
+
+    public void updateValueAnalogInput(String endpoint, Integer index, Double value){
+        outstationsService.getoOutstationData(endpoint).ifPresent(
+            outstationData -> {
+                outstationData.getOutstation().transaction(
+                    db -> {
+                        AnalogInput analogInput = new AnalogInput(UShort.valueOf(index), value, new Flags(Flag.ONLINE), TimeUtil.now());
+                        db.updateAnalogInput(analogInput, UpdateOptions.detectEvent());
+                    }
+                );
+            }
+        );
     }
 }

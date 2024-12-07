7bc5ca1f385a6829fe114917bf6079b5491a6bad
Budi Santoso <sebuah.email@gmail.com>
2024-12-07 13:17:41 +0700

    [patch]dashboard -fix auto/manual -mapping binary output operate

--- src/main/java/dev/boenkkk/simulator_outstation_dnp3/model/callback/ControlHandlerImpl.java
+++ src/main/java/dev/boenkkk/simulator_outstation_dnp3/model/callback/ControlHandlerImpl.java
@@ -1,5 +1,6 @@
 package dev.boenkkk.simulator_outstation_dnp3.model.callback;
 
+import dev.boenkkk.simulator_outstation_dnp3.model.MeasurementRequestModel;
 import dev.boenkkk.simulator_outstation_dnp3.service.DatapointService;
 import dev.boenkkk.simulator_outstation_dnp3.service.SocketIOService;
 import dev.boenkkk.simulator_outstation_dnp3.util.TimeUtil;
@@ -7,7 +8,9 @@ import io.stepfunc.dnp3.*;
 import lombok.extern.slf4j.Slf4j;
 import org.joou.UShort;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.joou.Unsigned.ushort;
 
@@ -19,6 +22,7 @@ public class ControlHandlerImpl implements ControlHandler {
     // NOTE: it use constructor injection coz cant be using DI
     private final DatapointService datapointService;
     private final SocketIOService socketIOService;
+
     public ControlHandlerImpl(DatapointService datapointService, SocketIOService socketIOService) {
         this.datapointService = datapointService;
         this.socketIOService = socketIOService;
@@ -32,7 +36,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public void endFragment(DatabaseHandle databaseHandle) {
-        String message = "endFragment databaseHandle:"+databaseHandle;
+        String message = "endFragment databaseHandle:" + databaseHandle;
         log.info(message);
     }
 
@@ -45,7 +49,7 @@ public class ControlHandlerImpl implements ControlHandler {
             commandStatus = CommandStatus.NOT_SUPPORTED;
         }
 
-        String message = "selectG12v1 group12Var1:"+group12Var1+", index:"+index+", databaseHandle:"+databaseHandle+", commandStatus:"+commandStatus;
+        String message = "selectG12v1 group12Var1:" + group12Var1 + ", index:" + index + ", databaseHandle:" + databaseHandle + ", commandStatus:" + commandStatus;
         log.info(message);
 
         return commandStatus;
@@ -53,7 +57,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus operateG12v1(Group12Var1 group12Var1, UShort index, OperateType opType, DatabaseHandle databaseHandle) {
-        CommandStatus commandStatus = null;
+        AtomicReference<CommandStatus> commandStatus = new AtomicReference<>();
         try {
             if (group12Var1.code.opType == OpType.LATCH_ON || group12Var1.code.opType == OpType.LATCH_OFF) {
                 boolean value = group12Var1.code.opType == OpType.LATCH_ON;
@@ -63,9 +67,9 @@ public class ControlHandlerImpl implements ControlHandler {
                         if (valueLocalRemote) {
                             Boolean valueCBOpenClose = datapointService.updateCBOpenClose(value);
                             socketIOService.broadcastToDefaultRoom("/cb-open-close", "listen", valueCBOpenClose);
-                            commandStatus = CommandStatus.SUCCESS;
+                            commandStatus.set(CommandStatus.SUCCESS);
                         } else {
-                            commandStatus = CommandStatus.LOCAL;
+                            commandStatus.set(CommandStatus.LOCAL);
                         }
                     }
                     case 1 -> {
@@ -73,9 +77,9 @@ public class ControlHandlerImpl implements ControlHandler {
                             datapointService.updateValueTapChanger(index.intValue());
                             Map<String, Object> dataTapChanger = datapointService.getDataTapChanger();
                             socketIOService.broadcastToDefaultRoom("/tap-changer", "listen", dataTapChanger);
-                            commandStatus = CommandStatus.SUCCESS;
+                            commandStatus.set(CommandStatus.SUCCESS);
                         } else {
-                            commandStatus = CommandStatus.NOT_SUPPORTED;
+                            commandStatus.set(CommandStatus.NOT_SUPPORTED);
                         }
                     }
                     case 2 -> {
@@ -84,32 +88,54 @@ public class ControlHandlerImpl implements ControlHandler {
                             datapointService.updateValueTapChanger(index.intValue());
                             Map<String, Object> dataTapChanger = datapointService.getDataTapChanger();
                             socketIOService.broadcastToDefaultRoom("/tap-changer", "listen", dataTapChanger);
-                            commandStatus = CommandStatus.SUCCESS;
+                            commandStatus.set(CommandStatus.SUCCESS);
                         } else {
-                            commandStatus = CommandStatus.NOT_SUPPORTED;
+                            commandStatus.set(CommandStatus.NOT_SUPPORTED);
                         }
                     }
                     case 3 -> {
                         datapointService.updateTapChangerAutoManual(value);
                         Map<String, Object> dataTapChangerUpdated = datapointService.getDataTapChanger();
                         socketIOService.broadcastToDefaultRoom("/tap-changer", "listen", dataTapChangerUpdated);
-                        commandStatus = CommandStatus.SUCCESS;
+                        commandStatus.set(CommandStatus.SUCCESS);
                     }
                     case 4 -> {
                         datapointService.updateTapChangerLocalRemote(value);
                         Map<String, Object> dataTapChangerUpdated = datapointService.getDataTapChanger();
                         socketIOService.broadcastToDefaultRoom("/tap-changer", "listen", dataTapChangerUpdated);
-                        commandStatus = CommandStatus.SUCCESS;
+                        commandStatus.set(CommandStatus.SUCCESS);
                     }
-                    default -> commandStatus = CommandStatus.OUT_OF_RANGE;
+                    // measurement auto / manual
+                    case 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 -> {
+                        List<MeasurementRequestModel> measurementRequestModels = MeasurementRequestModel.paramMeasurement();
+                        measurementRequestModels.stream()
+                            .filter(data -> index.intValue() == data.getIndexCmdAutoManual())
+                            .forEach(data -> {
+                                    try {
+                                        data.setUpdateValueAutoManual(value);
+                                        datapointService.updateMeasurementAutoManual(data);
+
+                                        Map<String, Object> dataMeasurement = datapointService.getValueMeasurement(data.getType(), data.getIndexValue(), data.getIndexAutoManual());
+                                        socketIOService.broadcastToDefaultRoom("/measurement", "listen", dataMeasurement);
+                                        commandStatus.set(CommandStatus.SUCCESS);
+                                    } catch (Exception e) {
+                                        log.error(e.getMessage());
+                                        commandStatus.set(CommandStatus.UNKNOWN);
+                                        throw new RuntimeException(e.getMessage());
+                                    }
+                                }
+                            );
+                    }
+                    default -> commandStatus.set(CommandStatus.OUT_OF_RANGE);
                 }
             } else {
-                commandStatus = CommandStatus.NOT_SUPPORTED;
+                commandStatus.set(CommandStatus.NOT_SUPPORTED);
             }
 
-            return commandStatus;
-        } catch (Exception e){
+            return commandStatus.get();
+        } catch (Exception e) {
             log.error(e.getMessage());
+            commandStatus.set(CommandStatus.HARDWARE_ERROR);
             throw new RuntimeException(e.getMessage());
         } finally {
             String message = "operateG12v1 group12Var1:" + group12Var1 + ", index:" + index + ", opType:" + opType + ", databaseHandle:" + databaseHandle + ", commandStatus:" + commandStatus;
@@ -119,7 +145,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus selectG41v1(int value, UShort index, DatabaseHandle databaseHandle) {
-        String message = "selectG41v1 value:"+value+", index:"+index+", databaseHandle:"+databaseHandle;
+        String message = "selectG41v1 value:" + value + ", index:" + index + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return selectAnalogOutput(index);
@@ -127,7 +153,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus operateG41v1(int value, UShort index, OperateType opType, DatabaseHandle databaseHandle) {
-        String message = "operateG41v1 value:"+value+", index:"+index+", opType:"+opType+", databaseHandle:"+databaseHandle;
+        String message = "operateG41v1 value:" + value + ", index:" + index + ", opType:" + opType + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return operateAnalogOutput(value, index, databaseHandle);
@@ -135,7 +161,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus selectG41v2(short value, UShort index, DatabaseHandle databaseHandle) {
-        String message = "selectG41v2 value:"+value+", index:"+index+", databaseHandle:"+databaseHandle;
+        String message = "selectG41v2 value:" + value + ", index:" + index + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return selectAnalogOutput(index);
@@ -143,7 +169,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus operateG41v2(short value, UShort index, OperateType opType, DatabaseHandle databaseHandle) {
-        String message = "operateG41v2 value:"+value+", index:"+index+", opType:"+opType+", databaseHandle:"+databaseHandle;
+        String message = "operateG41v2 value:" + value + ", index:" + index + ", opType:" + opType + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return operateAnalogOutput(value, index, databaseHandle);
@@ -151,7 +177,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus selectG41v3(float value, UShort index, DatabaseHandle databaseHandle) {
-        String message = "selectG41v3 value:"+value+", index:"+index+", databaseHandle:"+databaseHandle;
+        String message = "selectG41v3 value:" + value + ", index:" + index + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return selectAnalogOutput(index);
@@ -159,7 +185,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus operateG41v3(float value, UShort index, OperateType opType, DatabaseHandle databaseHandle) {
-        String message = "operateG41v3 value:"+value+", index:"+index+", opType:"+opType+", databaseHandle:"+databaseHandle;
+        String message = "operateG41v3 value:" + value + ", index:" + index + ", opType:" + opType + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return operateAnalogOutput(value, index, databaseHandle);
@@ -167,7 +193,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus selectG41v4(double value, UShort index, DatabaseHandle databaseHandle) {
-        String message = "selectG41v4 value:"+value+", index:"+index+", databaseHandle:"+databaseHandle;
+        String message = "selectG41v4 value:" + value + ", index:" + index + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return selectAnalogOutput(index);
@@ -175,7 +201,7 @@ public class ControlHandlerImpl implements ControlHandler {
 
     @Override
     public CommandStatus operateG41v4(double value, UShort index, OperateType opType, DatabaseHandle databaseHandle) {
-        String message = "operateG41v4 value:"+value+", index:"+index+", opType:"+opType+", databaseHandle:"+databaseHandle;
+        String message = "operateG41v4 value:" + value + ", index:" + index + ", opType:" + opType + ", databaseHandle:" + databaseHandle;
         log.info(message);
 
         return operateAnalogOutput(value, index, databaseHandle);
@@ -184,7 +210,7 @@ public class ControlHandlerImpl implements ControlHandler {
     private CommandStatus selectAnalogOutput(UShort index) {
         CommandStatus commandStatus = index.compareTo(ushort(10)) < 0 ? CommandStatus.SUCCESS : CommandStatus.NOT_SUPPORTED;
 
-        String message = "selectAnalogOutput index:"+index+", commandStatus:"+commandStatus;
+        String message = "selectAnalogOutput index:" + index + ", commandStatus:" + commandStatus;
         log.info(message);
 
         return commandStatus;
@@ -204,7 +230,7 @@ public class ControlHandlerImpl implements ControlHandler {
             commandStatus = CommandStatus.NOT_SUPPORTED;
         }
 
-        String message = "operateAnalogOutput value:"+value+", index:"+index+", databaseHandle:"+databaseHandle+", commandStatus:"+commandStatus;
+        String message = "operateAnalogOutput value:" + value + ", index:" + index + ", databaseHandle:" + databaseHandle + ", commandStatus:" + commandStatus;
         log.info(message);
 
         return commandStatus;
